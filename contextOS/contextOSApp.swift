b4c73f5ce63c3ba68b433cd30371cc92030e158b
Spencer Dearman <dearmanspencer@gmail.com>
2026-04-15 19:05:30 -0500

    Initial Commit

---
+++ contextOS/contextOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  contextOSApp.swift
+//  contextOS
+//
+//  Created by Spencer Dearman on 4/15/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct contextOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
